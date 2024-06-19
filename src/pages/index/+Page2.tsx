ffadb7f6ec012dfee5230c0b7d7b0d65286299e5
Victor Alvelais <6127592+victoralvelais@users.noreply.github.com>
2024-06-19 00:16:02 -0500

    add page files

--- src/pages/index/+Page2.tsx
+++ src/pages/index/+Page2.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Profile from '../components/profile/main'
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import Profile from '../../components/profile/main'
 import 'antd/dist/antd.css';
 import "./index.css"
 
